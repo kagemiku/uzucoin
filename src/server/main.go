3960ec27144d0f3f5179afca1c0e8b05befa0f4b
kagemiku <kagemiku@gmail.com>
2018-12-16 23:27:19 +0900

    Add files for first build

---
+++ src/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/kagemiku/uzucoin/src/server/pb"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUzucoinServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
