9e3d27d67bbdda29cc3026a4fc31b2cadea9986b
kagemiku <kagemiku@gmail.com>
2018-12-17 23:37:51 +0900

    Setup Makefile and main.go for executing

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+GO = go
 PROTOC = protoc
 
 SERVICE = uzucoin
@@ -5,5 +6,18 @@ PROTO_DIR = protobuf
 SERVER_DIR = src/server
 PROTO_SERVER_DIR = $(SERVER_DIR)/pb
 
+SERVER_SOURCES = main.go
+
+.DEFAULT_GOAL = run
+
 proto:
 	@$(PROTOC) -I $(PROTO_DIR)/ $(PROTO_DIR)/$(SERVICE).proto --go_out=plugins=grpc:$(PROTO_SERVER_DIR)
+
+build: proto
+	@$(GO) build -o $(SERVER_DIR)/$(SERVICE) $(SERVER_DIR)/$(SERVER_SOURCES)
+
+run: build
+	@./$(SERVER_DIR)/$(SERVICE)
+
+clean:
+	@rm $(SERVER_DIR)/$(SERVICE)
--- src/server/main.go
+++ src/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
@@ -15,6 +16,30 @@ const (
 )
 
 type server struct{}
+
+func (s *server) AddTransaction(ctx context.Context, in *pb.TransactionRequest) (*pb.AddTransactionResponse, error) {
+	return nil, nil
+}
+
+func (s *server) GetTask(ctx context.Context, in *pb.GetTaskRequest) (*pb.Task, error) {
+
+	return nil, nil
+}
+
+func (s *server) ResolveNonce(ctx context.Context, in *pb.Nonce) (*pb.ResolveNonceResponse, error) {
+
+	return nil, nil
+}
+
+func (s *server) GetHistory(ctx context.Context, in *pb.GetHistoryRequest) (*pb.History, error) {
+
+	return nil, nil
+}
+
+func (s *server) GetBalance(ctx context.Context, in *pb.GetBalanceRequest) (*pb.Balance, error) {
+
+	return nil, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", port)
