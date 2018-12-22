7b67da54cd287bae74a6809aa4c3b32cc60cd176
kagemiku <kagemiku@gmail.com>
2018-12-22 15:25:23 +0900

    Change rpc declaration order

--- src/server/handler.go
+++ src/server/handler.go
@@ -10,20 +10,6 @@ type server struct {
 	usecase *uzucoinUsecase
 }
 
-func (s *server) AddTransaction(ctx context.Context, in *pb.TransactionRequest) (*pb.AddTransactionResponse, error) {
-	return nil, nil
-}
-
-func (s *server) GetTask(ctx context.Context, in *pb.GetTaskRequest) (*pb.Task, error) {
-
-	return nil, nil
-}
-
-func (s *server) ResolveNonce(ctx context.Context, in *pb.Nonce) (*pb.ResolveNonceResponse, error) {
-
-	return nil, nil
-}
-
 func (s *server) RegisterUser(ctx context.Context, in *pb.RegisterUserRequest) (*pb.RegisterUserResponse, error) {
 	return nil, nil
 }
@@ -34,6 +20,20 @@ func (s *server) GetHistory(ctx context.Context, in *pb.GetHistoryRequest) (*pb.
 }
 
 func (s *server) GetBalance(ctx context.Context, in *pb.GetBalanceRequest) (*pb.Balance, error) {
+
+	return nil, nil
+}
+
+func (s *server) AddTransaction(ctx context.Context, in *pb.TransactionRequest) (*pb.AddTransactionResponse, error) {
+	return nil, nil
+}
+
+func (s *server) GetTask(ctx context.Context, in *pb.GetTaskRequest) (*pb.Task, error) {
+
+	return nil, nil
+}
+
+func (s *server) ResolveNonce(ctx context.Context, in *pb.Nonce) (*pb.ResolveNonceResponse, error) {
 
 	return nil, nil
 }
