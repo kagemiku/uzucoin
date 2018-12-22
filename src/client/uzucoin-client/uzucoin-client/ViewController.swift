d1ba4dd2ce605564b0453c3c4e423e16ee01fa44
kagemiku <kagemiku@gmail.com>
2018-12-23 00:07:23 +0900

    Add empty xcode project

---
+++ src/client/uzucoin-client/uzucoin-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uzucoin-client
+//
+//  Created by KAGE on 2018/12/22.
+//  Copyright © 2018 kagemiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
