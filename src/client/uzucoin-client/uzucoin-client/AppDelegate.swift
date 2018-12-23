43b027544ebd7a45763892b016f55071078e9d50
kagemiku <kagemiku@gmail.com>
2018-12-23 11:58:55 +0900

    Add RootVC

--- src/client/uzucoin-client/uzucoin-client/AppDelegate.swift
+++ src/client/uzucoin-client/uzucoin-client/AppDelegate.swift
@@ -18,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         gRPC.initialize()
 
+        let vc = RootViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+
         return true
     }
 
---
+++ src/client/uzucoin-client/uzucoin-client/View/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  uzucoin-client
+//
+//  Created by Akira Fukunaga on 2018/12/23.
+//  Copyright © 2018 kagemiku. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
